66cd8ef8f58deac3180a697e94f2f2dec35a0e16
Simon Jung <vicevil4@gmail.com>
2022-05-09 20:48:48 +0900

    Initial Commit

---
+++ Shared/sample_webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_webviewApp.swift
+//  Shared
+//
+//  Created by aincc on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct sample_webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
